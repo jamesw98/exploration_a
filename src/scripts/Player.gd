dc95348c38533b814da3d8b5f95043ca4d9d7719
jamesw98 <xeekatar@gmail.com>
2021-06-23 20:07:55 -0400

    stamina scrapped, dash implemented, floor color changed

--- src/scripts/Player.gd
+++ src/scripts/Player.gd
@@ -1,36 +1,44 @@
 extends KinematicBody2D
 
 export var base_speed := 175
-export var max_stamina := 200
+export var base_dash_time := 50
+export var base_dash_cooldown := 200
 
+var dash_time := base_dash_time
+var dash_cooldown := base_dash_cooldown
+
+var dashing := false
+var cooling_down := false
 var speed := base_speed
-var stamina := max_stamina
-var running := false
-var out_of_stam := false
 
 func _physics_process(delta) -> void:
 	var direction := calculate_move_direction()
 	
-	if Input.is_action_just_pressed("run"):
-		running = true
-		speed *= 2
-	elif Input.is_action_just_released("run") or out_of_stam:
-		out_of_stam = false
-		running = false
-		speed = base_speed
-		
-	if (running):
-		stamina -= 1
-	elif (stamina < max_stamina): 
-		stamina += 1
-		
-	print(stamina)
-		
-	if (stamina < 0):
-		running = false
-		out_of_stam = true
+	if Input.is_action_just_pressed("dash") and not dashing:
+		toggle_dot()
+		speed = speed * 3
+		dashing = true
 	
+	if dashing:
+		dash_time -= 1
+	
+	if dash_time <= 0:
+		speed = base_speed
+		cooling_down = true
+		dashing = false
+	
+	if cooling_down:
+		dash_cooldown -= 1
+		if (dash_cooldown <= 0):
+			toggle_dot()
+			cooling_down = false
+			dash_cooldown = base_dash_cooldown
+			dash_time = base_dash_time
+		
 	move_and_slide(direction * speed)
+	
+func toggle_dot():
+	get_node("dash").visible = not get_node("dash").visible
 	
 func calculate_move_direction() -> Vector2:
 	return Vector2(
