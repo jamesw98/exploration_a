b525d59d0180124cd5b5038a9517d13eab7b4e41
jamesw98 <xeekatar@gmail.com>
2021-06-24 21:52:21 -0400

    first journal entry opening and closing

--- src/scripts/Player.gd
+++ src/scripts/Player.gd
@@ -19,10 +19,18 @@ var second_teleport := false
 onready var sprite := get_node("player_anim")
 var last_dir := "walk_right"
 
+var current_journal := -1
+var reading := false
+
+var PAGES = ["j1", "j2"]
+
 # runs every frame
 func _physics_process(delta) -> void:
+	var direction = Vector2.ZERO
+	
 	# gets the direction the player is moving
-	var direction := calculate_move_direction()
+	if not reading:
+		direction = calculate_move_direction()
 	
 	# animate the player sprite
 	animate(direction)
@@ -30,14 +38,14 @@ func _physics_process(delta) -> void:
 	# checks if there is teleportation to be done
 	check_teleport(direction)
 	
+	if current_journal > 0:
+		check_journal()
+	
 	# if the dash button is pressed, and the player isn't already dashing, start dashing
 	if Input.is_action_just_pressed("dash") and not dashing:
 		disable_dot() # turns off the stamina dot
 		speed = speed * 2 # adjusts movement speed
 		dashing = true
-	
-	if (Input.is_action_just_pressed("interact")):
-		global_position.x = global_position.x + 100
 	
 	# only dash for a certain period of time
 	if dashing:
@@ -62,10 +70,18 @@ func _physics_process(delta) -> void:
 			dash_time = base_dash_time
 			
 	move_and_slide(direction * speed)
+	
+func check_journal():
+	if Input.is_action_just_pressed("interact") and not reading:
+		reading = true
+		show_journal(current_journal)
+	elif Input.is_action_just_pressed("interact") and reading:
+		close_journal(current_journal)
+		reading = false
 
 func check_teleport(direction):
-	#if DEBUG:
-	#	print(global_position)
+	if DEBUG:
+		print(global_position)
 	
 	# initial hallway
 	if global_position.x >= 1190 and not first_teleport:
@@ -100,6 +116,14 @@ func animate(direction: Vector2):
 	else:
 		sprite.play("walk_down")
 		sprite.stop()
+		
+func show_journal(journal_number):
+	get_node(PAGES[journal_number - 1]).visible = true
+	get_node("close_message").visible = true
+	
+func close_journal(journal_number):
+	get_node(PAGES[journal_number - 1]).visible = false
+	get_node("close_message").visible = false
 
 # turns the dash dot off
 func disable_dot():
@@ -118,6 +142,8 @@ func calculate_move_direction() -> Vector2:
 
 func _on_journal1_body_entered(body):
 	get_parent().get_node("journal_1/interact").visible = true
+	current_journal = 1
 
 func _on_journal1_body_exited(body):
 	get_parent().get_node("journal_1/interact").visible = false
+	current_journal = 0
